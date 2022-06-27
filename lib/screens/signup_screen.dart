3927062c770763f33640f3027713377456ced256
Harsh Pokarna <f20200754@pilani.bits-pilani.ac.in>
2022-06-27 12:20:20 +0530

    signup ui created

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -3,21 +3,25 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram_clone/utils/colors.dart';
 import 'package:instagram_clone/widgets/text_field_input.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+class SignUpScreen extends StatefulWidget {
+  SignUpScreen({Key? key}) : super(key: key);
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<SignUpScreen> createState() => _SignUpScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _SignUpScreenState extends State<SignUpScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _bioController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
 
   @override
   void dispose() {
     _emailController.dispose();
     _passwordController.dispose();
+    _bioController.dispose();
+    _usernameController.dispose();
     super.dispose();
   }
 
@@ -38,6 +42,38 @@ class _LoginScreenState extends State<LoginScreen> {
               height: 64,
             ),
             const SizedBox(height: 64),
+            Stack(
+              children: [
+                const CircleAvatar(
+                  radius: 64,
+                  backgroundImage: NetworkImage(
+                      'https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'),
+                ),
+                Positioned(
+                  bottom: -10,
+                  right: -10,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.add_a_photo),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 32),
+            // text field for user name
+            TextFieldInput(
+              textEditingController: _usernameController,
+              hintText: 'User Name',
+              textInputType: TextInputType.text,
+            ),
+            const SizedBox(height: 20),
+            // text filed for bio
+            TextFieldInput(
+              textEditingController: _bioController,
+              hintText: 'Bio',
+              textInputType: TextInputType.text,
+            ),
+            const SizedBox(height: 20),
             // text field for email
             TextFieldInput(
               textEditingController: _emailController,
@@ -45,14 +81,13 @@ class _LoginScreenState extends State<LoginScreen> {
               textInputType: TextInputType.emailAddress,
             ),
             const SizedBox(height: 20),
-            // text field for password
+            // text filed for password
             TextFieldInput(
               textEditingController: _passwordController,
               hintText: 'Password',
               isPass: true,
               textInputType: TextInputType.text,
             ),
-            // login button
             GestureDetector(
               onTap: () {},
               child: Container(
@@ -64,7 +99,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     borderRadius: BorderRadius.all(Radius.circular(4)),
                     color: blueColor),
                 child: const Text(
-                  'Log In',
+                  'Sign Up',
                   style: TextStyle(fontWeight: FontWeight.bold),
                 ),
               ),
@@ -73,13 +108,13 @@ class _LoginScreenState extends State<LoginScreen> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text('Do not have an account?'),
+                const Text('Already have an account?'),
                 GestureDetector(
                   onTap: () {},
                   child: const Padding(
                     padding: EdgeInsets.all(8.0),
                     child: Text(
-                      'Sign up.',
+                      'Login.',
                       style: TextStyle(
                         color: blueColor,
                         fontWeight: FontWeight.bold,
