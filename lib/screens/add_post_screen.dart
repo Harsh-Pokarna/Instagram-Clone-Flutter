e1419345ad811e3cf04582c4a2c1a6a4228ef45d
Harsh Pokarna <f20200754@pilani.bits-pilani.ac.in>
2022-06-30 21:21:06 +0530

    image selection for post added

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,5 +1,11 @@
+import 'dart:typed_data';
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:instagram_clone/models/user.dart';
+import 'package:instagram_clone/providers/user_provider.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/utils/utils.dart';
 
 class AddPostScreen extends StatefulWidget {
   AddPostScreen({Key? key}) : super(key: key);
@@ -9,16 +15,69 @@ class AddPostScreen extends StatefulWidget {
 }
 
 class _AddPostScreenState extends State<AddPostScreen> {
+  Uint8List? _file;
+  final _captionController = TextEditingController();
+
+  void selecteImage() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Select Image'),
+        content: SizedBox(
+          height:104,
+          child: Column(
+            children: [
+              GestureDetector(
+                onTap: () async {
+                  var file = await pickImage(ImageSource.gallery);
+                  if(file != null) {
+                    setState(() {
+                      _file = file;
+                    });
+                    Navigator.of(context).pop();
+                  }
+                },
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  width: MediaQuery.of(context).size.width,
+                  child: const Text('Gallery'),
+                ),
+              ),
+              GestureDetector(
+                onTap: () async {
+                  var file = await pickImage(ImageSource.camera);
+                  if(file != null) {
+                    setState(() {
+                      _file = file;
+                    });
+                    Navigator.of(context).pop();
+                  }
+                },
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  width: MediaQuery.of(context).size.width,
+                  child: const Text('Camera'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // return Center(
-    //   child: IconButton(
-    //     icon: const Icon(Icons.upload),
-    //     color: primaryColor,
-    //     onPressed: () {},
-    //   ),
-    // );
-    return Scaffold(
+  User user = Provider.of<UserProvider>(context, listen: false).getUser;
+
+    return _file == null ?  Center(
+      child: IconButton(
+        icon: const Icon(Icons.upload),
+        color: primaryColor,
+        onPressed: selecteImage,
+      ),
+    ) : 
+     Scaffold(
       appBar: AppBar(
         backgroundColor: mobileBackgroundColor,
         leading: IconButton(
@@ -46,9 +105,8 @@ class _AddPostScreenState extends State<AddPostScreen> {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const CircleAvatar(
-                backgroundImage: NetworkImage(
-                    'https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'),
+              CircleAvatar(
+                backgroundImage: NetworkImage(user.photoUrl),
               ),
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.45,
@@ -58,21 +116,18 @@ class _AddPostScreenState extends State<AddPostScreen> {
                     border: InputBorder.none,
                   ),
                   maxLines: 8,
+                  controller: _captionController,
                 ),
               ),
               SizedBox(
                 height: 45,
                 width: 45,
-                child: AspectRatio(
-                  aspectRatio: 5 / 4,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(
-                            'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cG9ydHJhaXR8ZW58MHx8MHx8&w=1000&q=80'),
-                        fit: BoxFit.contain,
-                        alignment: FractionalOffset.topCenter,
-                      ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: MemoryImage(_file!),
+                      fit: BoxFit.contain,
+                      alignment: FractionalOffset.topCenter,
                     ),
                   ),
                 ),
