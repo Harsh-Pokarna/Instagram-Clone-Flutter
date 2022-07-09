93e669742d6fef8a0205e8ee737af38ec67c6272
Harsh Pokarna <f20200754@pilani.bits-pilani.ac.in>
2022-07-09 14:19:27 +0530

    searching part made

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,68 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
+
+class SearchScreen extends StatefulWidget {
+  SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final TextEditingController _searchController = TextEditingController();
+  bool _isShowUsers = false;
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextFormField(
+          controller: _searchController,
+          decoration: const InputDecoration(
+            hintText: 'Search for user',
+          ),
+          onFieldSubmitted: (_) {
+            setState(() {
+              _isShowUsers = true;
+            });
+          },
+        ),
+        backgroundColor: mobileBackgroundColor,
+      ),
+      body: _isShowUsers ? FutureBuilder(
+        future: FirebaseFirestore.instance
+            .collection('users')
+            .where('username', isGreaterThanOrEqualTo: _searchController.text)
+            .get(),
+        builder:
+            (_, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return ListView.builder(
+              itemBuilder: (_, index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                      backgroundImage: NetworkImage(snapshot.data!.docs[index]['photoUrl'])),
+                  title: Text(snapshot.data!.docs[index]['username']),
+                );
+              },
+              itemCount: snapshot.data!.docs.length,
+            );
+          }
+        },
+      ) : Text('Posts')
+    );
+  }
+}
+ 
